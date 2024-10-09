1f77201e7d08ea97f342435d240b31ad330f314d
jjaison26 <jj89177@mail.harpercollege.edu>
2024-10-09 16:20:43 -0500

    imported peek
    Included code for peek

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+using namespace std;
+class Stack {
+  private:
+  vector<int> v;
+  public:
+  void push(int element) {
+      
+  }
+  void pop() {
+      
+  }
+  
+  int peek(){
+      return this->v[(this->v.size() -1)];
+  }
+  
+};
+
+int main() {
+
+
+    return 0;
+}
