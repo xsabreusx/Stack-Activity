b26124d7ecd854148d1fb85787e426ad5949b9ce
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-10-09 18:03:22 -0500

    updated to template class, added extra test to check strings

--- main.cpp
+++ main.cpp
@@ -3,53 +3,68 @@
 #include <vector>
 using namespace std;
 
+template <typename T>
 class Stack {
   private:
-  vector<int> v;
+  
+  vector<T> v;
   
   public:
   
   Stack(){}
 
-  Stack(vector<int> v) {
+  Stack(vector<T> v) {
       this->v = v;
   }
 
-  void push(int element) {
+  void push(T element) {
       this->v.push_back(element);
   }
 
-  int pop() {
-    if (v.size()==0)
+  T pop() 
+  {
+
+    if (v.size() == 0) 
     {
-      return -1;
-    }
-    else {
-      int p = 0;
-      p = v[v.size()-1];
+      throw runtime_error("Stack is empty. Cannot pop element.");
+    } 
+    else 
+    {
+      T p = v[v.size() - 1];
       v.pop_back();
       return p;
     }
-    
+  }
+  
+  T peek(){
+    if (v.size() == 0) {
+      throw runtime_error("Stack is empty. Cannot peek.");
+    }
+    return this->v[this->v.size() - 1];
+  }
 
-  }
-  
-  int peek(){
-      return this->v[(this->v.size() -1)];
-  }
-  
 };
 
 int main() 
 {
     
     
-  Stack testStack;
+  Stack <int> testStack;
 
   testStack.push(1);
 
   assert(testStack.pop() == 1);
   
+  vector <int> v = {2, 3, 5};
+  Stack <int> testStack2(v);
+  assert(testStack2.pop()== 5);
+
+  Stack <string> stringStack;   //testing the case of strings
+  stringStack.push("Howdy");
+  stringStack.push("Partner");
+  assert(stringStack.pop() == "Partner");
+  assert(stringStack.peek() == "Howdy");
+
   cout <<"\nTests passed.";
 
 
